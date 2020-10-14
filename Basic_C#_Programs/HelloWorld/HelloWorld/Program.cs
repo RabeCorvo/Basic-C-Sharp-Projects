6285272b3ce8618c1f32a86a1060ac808aae765c
Samuel Breidenbach <sambreidenbach@gmail.com>
2020-10-14 11:26:07 -0700

    Basic_C#_Prjoect_File
    All TTA C# projects

---
+++ Basic_C#_Programs/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+   class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("I said to myself, \"I want to get better!\"");
+            Console.ReadLine();
+        }
+    }
+
