ea6f7d7c0796d01532b51d074672d9b5bb221479
Samuel Breidenbach <sambreidenbach@gmail.com>
2020-10-16 15:17:04 -0700

    Added Loops project and WhileDoWhileAssignment project

--- Basic_C#_Programs/Branching/Branching/Program.cs
+++ Basic_C#_Programs/Branching/Branching/Program.cs
@@ -10,12 +10,31 @@ namespace Branching
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your favorite number?");
-            int favNum = Convert.ToInt32(Console.ReadLine());
 
-            string result = favNum == 12 ? "You have an awesome favorite number." : "You do not have an awesome favorite number.";
+            Console.WriteLine("Masses of evil monsters surround you.  You see three objects in front of you.\nAn axe, a sword, and a pen.  Which do you choose?");
+            Console.WriteLine("Press 1 for the Axe, 2 for the sword, and 3 for the pen.");
+            int userChoice = Convert.ToInt32(Console.ReadLine());
+            switch (userChoice)
+            {
+                case 1:
+                    Console.WriteLine("You chose the axe.  The hordes of evil will stand little chance against you.");
+                    break;
+                case 2:
+                    Console.WriteLine("You chose the sword. Balanced and sturdy, you will have little trouble\nsurviving against the evil mobs.");
+                    break;
+                case 3:
+                    Console.WriteLine("The pen?  You chose the pen?  Ugh...the evil monster instantly jump and devour you.\nBut the world isn't too sad because you chose the pen and obviously had it coming.");
+                    break;
+            }
+            Console.ReadLine();
 
-            Console.WriteLine(result);
+
+            //Console.WriteLine("What is your favorite number?");
+            //int favNum = Convert.ToInt32(Console.ReadLine());
+
+            //string result = favNum == 12 ? "You have an awesome favorite number." : "You do not have an awesome favorite number.";
+
+            //Console.WriteLine(result);
             
             //int roomTemperature = 70;
 
