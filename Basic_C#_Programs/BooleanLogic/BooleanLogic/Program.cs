c78587870734ca2fde830bc0e089f24dc185b0b0
Samuel Breidenbach <sambreidenbach@gmail.com>
2020-10-16 10:41:12 -0700

    Added BooleanLogic.sln and BooleanLogicAssignment.sln

---
+++ Basic_C#_Programs/BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            // And
+            //Console.WriteLine(true && false); // results False
+            //Console.WriteLine(true && true); // results True
+            //Console.WriteLine(false && false); // results False
+
+            // Or
+            //Console.WriteLine(true || true);  // results True
+            //Console.WriteLine(true || false);  // results True
+            //Console.WriteLine(false || false);  // results False
+
+            // Equal
+            //Console.WriteLine(true == true);  // results True
+            //Console.WriteLine(true == false);  // results False
+            //Console.WriteLine(false == false);  // results True
+
+            // Not equal
+            //Console.WriteLine(true != true);  // results False
+            //Console.WriteLine(true != false);  // results True
+            //Console.WriteLine(false != false);  // results False
+
+
+            // X Or 
+            //Console.WriteLine(true ^ true); // results False
+            //Console.WriteLine(true ^ false); // results True
+            //Console.WriteLine(false ^ false); // results False
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/BooloeanLogicAssignment/BooloeanLogicAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooloeanLogicAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            string userAgeStr = Console.ReadLine();
+            int userAgeInt = Convert.ToInt32(userAgeStr);
+
+            Console.WriteLine("Have you ever had a DUI? Please enter 'true' or 'false'.");
+            string userDUIStr = Console.ReadLine();
+            bool userDUIBool = Convert.ToBoolean(userDUIStr);
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string userTicketStr = Console.ReadLine();
+            int userTicketInt = Convert.ToInt32(userTicketStr);
+
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(userAgeInt > 15 && userDUIBool == false && userTicketInt < 3);
+
+            Console.ReadLine();
+                
+
+        }
+    }
+}
