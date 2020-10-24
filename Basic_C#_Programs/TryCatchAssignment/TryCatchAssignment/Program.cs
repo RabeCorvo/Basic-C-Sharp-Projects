fd611b994e5612d9ca716d14b4f17afa48465e98
Samuel Breidenbach <sambreidenbach@gmail.com>
2020-10-24 13:26:14 -0700

    Added ExceptionHandling project and TryCatchAssignment project

---
+++ Basic_C#_Programs/TryCatchAssignment/TryCatchAssignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                List<int> numberList = new List<int>() { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+                Console.WriteLine("Pick a number to divide by.");
+                int divideBy = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < numberList.Count; i++)
+                {
+                    Console.WriteLine(numberList[i] / divideBy);
+                }
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("All calculations are complete.");
+            Console.ReadLine();
+        }
+    }
+}
