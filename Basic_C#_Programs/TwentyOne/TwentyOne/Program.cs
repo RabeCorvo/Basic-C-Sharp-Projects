09609359aa767e4c2658cab4c6851f11462d3326
Samuel Breidenbach <sambreidenbach@gmail.com>
2020-10-25 15:29:02 -0700

    Added AbstractClassAssignment, updated TwentyOne Assignment

--- Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
@@ -11,20 +11,18 @@ namespace TwentyOne
         static void Main(string[] args)
         {
             TwentyOneGame game = new TwentyOneGame();
-            game.Players = new List<string>() { "Sam", "Bill", "Joe" };
+            game.Players = new List<string>() { "Sam", "Bill", "Tom" };
             game.ListPlayers();
-            game.Play();
             Console.ReadLine();
+            Deck deck = new Deck();
+            deck.Shuffle(100);
 
-            //Deck deck = new Deck();
-            //deck.Shuffle(100);
-
-            //foreach (Card card in deck.Cards)
-            //{
-            //    Console.WriteLine(card.Face + " of " + card.Suit);
-            //}
-            //Console.WriteLine(deck.Cards.Count);
-            //Console.ReadLine();
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            Console.WriteLine(deck.Cards.Count);
+            Console.ReadLine();
         }
     }
 }
