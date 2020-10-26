eefc0efcf481ba7a5a3e057e49f6318c05b02ba9
Samuel Breidenbach <sambreidenbach@gmail.com>
2020-10-26 16:23:10 -0700

    Added EnumAssignment project

---
+++ Basic_C#_Programs/EnumAssignment/EnumAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Days days = new Days();
+            Console.WriteLine("Please enter the current day of the week.");
+            string userDay = Console.ReadLine();
+            try
+            {
+                Enum.Parse(typeof(Days.DaysOfTheWeek), userDay);
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an ACTUAL day of the week.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
