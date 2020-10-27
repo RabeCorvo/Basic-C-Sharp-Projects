657865382f9368dde86a65cb615ea7837f80a9d9
Samuel Breidenbach <sambreidenbach@gmail.com>
2020-10-27 11:19:26 -0700

    Added StructAssignment project

---
+++ Basic_C#_Programs/StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 123456789;
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+
+        }
+    }
+}
