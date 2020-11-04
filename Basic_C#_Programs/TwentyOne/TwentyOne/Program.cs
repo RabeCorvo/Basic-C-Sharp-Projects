4dfd4e0b8db4b920670dd800a9417cf50adc1165
Samuel Breidenbach <sambreidenbach@gmail.com>
2020-11-04 14:50:29 -0800

    Added AdditionalFeaturesAssignment, updated TwentyOne project

--- Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using Casino;
+using Casino.TwentyOne;
 
 namespace TwentyOne
 {
@@ -11,8 +13,11 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
+            const string casinoName = "Dinosaur Motel and Casino";
 
-            Console.WriteLine("Welcome to the Dinosaur Motel and Casino.  Let's start by telling me your name.");
+           
+
+            Console.WriteLine("Welcome to the {0}.  Let's start by telling me your name.", casinoName);
             string playerName = Console.ReadLine();
             Console.WriteLine("And how much money did you bring today?");
             int bank = Convert.ToInt32(Console.ReadLine());
@@ -21,6 +26,11 @@ namespace TwentyOne
             if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
             {
                 Player player = new Player(playerName, bank);
+                player.Id = Guid.NewGuid();
+                using (StreamWriter file = new StreamWriter(@"C:\Users\ywing\Desktop\log.txt", true))
+                {
+                    file.WriteLine(player.Id);
+                }
                 Game game = new TwentyOneGame();
                 game += player;
                 player.isActivelyPlaying = true;
