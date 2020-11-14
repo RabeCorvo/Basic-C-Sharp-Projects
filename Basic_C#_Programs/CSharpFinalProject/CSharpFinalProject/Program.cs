7ded2b1898b5f20bfc9b8956468e73c0b88f9669
Samuel Breidenbach <sambreidenbach@gmail.com>
2020-11-14 12:35:02 -0800

    Add CSharpFinalProject

---
+++ Basic_C#_Programs/CSharpFinalProject/CSharpFinalProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFinalProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var schoolContext = new SchoolContext())
+            {
+                var student = new Student() { FirstName = "Sam", LastName = "Breidenbach" };
+
+                schoolContext.Students.Add(student);
+                schoolContext.SaveChanges();                
+            }
+        }
+    }
+}
